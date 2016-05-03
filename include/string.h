dbd26ebc023acc143c8ce3599736b74f5be27a0e
chris <chris@hichris.com>
2016-05-03 19:18:03 -0400

    Initial commit.

---
+++ include/string.h
@@ -0,0 +1,13 @@
+/*
+ * string.h
+ *
+ *  Created on: May 3, 2016
+ *      Author: chris
+ */
+
+#ifndef INCLUDE_STRING_H_
+#define INCLUDE_STRING_H_
+
+typedef const char* String;
+
+#endif /* INCLUDE_STRING_H_ */
---
+++ src/string.cpp
@@ -0,0 +1,8 @@
+/*
+ * string.cpp
+ *
+ *  Created on: May 3, 2016
+ *      Author: chris
+ */
+
+#include <string.h>
